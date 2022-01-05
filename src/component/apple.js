929b4f5031daf02219bbe36495e151d647636ba6
Tuguldur <tuguldur@iMac-2021-09s-iMac.local>
2022-01-05 16:18:48 +0800

    snake

---
+++ src/component/apple.js
@@ -0,0 +1,9 @@
+import React from "react";  
+import "../App.css"
+
+const Apple = () => {
+    return <div>
+        <div className="board center" style={{ backgroundColor: "red" }}></div>
+    </div>
+}
+export default Apple;
---
+++ src/component/snake.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import "../App.css"
+
+const Snake = ({Arr}) => {
+    return <div>
+        <div className="board" style={{ backgroundColor: "green" }} />
+    </div>
+}
+export default Snake;
